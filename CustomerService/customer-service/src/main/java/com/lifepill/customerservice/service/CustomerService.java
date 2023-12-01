018b1946dc257c70a78018a79396a4208fd4211a
Inupa Vihan <inupavihan9@gmail.com>
2023-12-01 11:20:23 +0530

    Add error handling for missing parameters
    Add new custom exception for missing parameters

--- CustomerService/customer-service/src/main/java/com/lifepill/customerservice/service/CustomerService.java
+++ CustomerService/customer-service/src/main/java/com/lifepill/customerservice/service/CustomerService.java
@@ -2,6 +2,7 @@ package com.lifepill.customerservice.service;
 
 import com.lifepill.customerservice.model.Customer;
 import com.lifepill.customerservice.repo.CustomerRepository;
+import com.lifepill.customerservice.util.MissingParameterException;
 import com.lifepill.customerservice.util.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -31,6 +32,46 @@ public class CustomerService {
 
     //add new customer
     public Customer addNewCustomer(Customer customer){
+        //Empty full name error handling
+        if(customer.getCustomerFullName().isEmpty()){
+            throw new MissingParameterException("Full Name cannot be Empty");
+        }
+
+        //Empty email error handling
+        if(customer.getCustomerEmail().isEmpty()){
+            throw new MissingParameterException("Email cannot be Empty");
+        }
+
+        //Empty mobile number error handling
+        if(customer.getCustomerMobileNumber().isEmpty()){
+            throw new MissingParameterException("Mobile Number cannot be Empty");
+        }
+
+        //Empty password error handling
+        if(customer.getCustomerPassword().isEmpty()){
+            throw new MissingParameterException("Password cannot be Empty");
+        }
+
+        //Empty street name error handling
+        if(customer.getCustomerAddressStreet().isEmpty()){
+            throw new MissingParameterException("Address Street cannot be Empty");
+        }
+
+        //Empty city name error handling
+        if(customer.getCustomerAddressCity().isEmpty()){
+            throw new MissingParameterException("Address City cannot be Empty");
+        }
+
+        //Empty district error handling
+        if(customer.getCustomerAddressDistrict().isEmpty()){
+            throw new MissingParameterException("Address District cannot be Empty");
+        }
+
+        //Empty NIC number error handling
+        if(customer.getCustomerNIC().isEmpty()){
+            throw new MissingParameterException("NIC Number cannot be Empty");
+        }
+
         return customerRepository.save(customer);
     }
 
@@ -38,29 +79,71 @@ public class CustomerService {
     public Customer updateCustomer(Long id, Customer updatedCustomer){
         Optional<Customer> customer = customerRepository.findById(id);
 
+        // Customer Id not found error handling
         if(customer.isEmpty()){
             throw new ResourceNotFoundException("Customer with ID " + id + " not found.");
-        }else{
-            Customer existingCustomer = customer.get();
-
-            existingCustomer.setCustomerFullName(updatedCustomer.getCustomerFullName());
-            existingCustomer.setCustomerEmail(updatedCustomer.getCustomerEmail());
-            existingCustomer.setCustomerMobileNumber(updatedCustomer.getCustomerMobileNumber());
-            existingCustomer.setCustomerPassword(updatedCustomer.getCustomerPassword());
-            existingCustomer.setCustomerAddressStreet(updatedCustomer.getCustomerAddressStreet());
-            existingCustomer.setCustomerAddressCity(updatedCustomer.getCustomerAddressCity());
-            existingCustomer.setCustomerAddressDistrict(updatedCustomer.getCustomerAddressDistrict());
-            existingCustomer.setCustomerNIC(updatedCustomer.getCustomerNIC());
-            existingCustomer.setCustomerDateOfBirth(updatedCustomer.getCustomerDateOfBirth());
-
-            return customerRepository.save(existingCustomer);
         }
+
+        //Empty full name error handling
+        if(updatedCustomer.getCustomerFullName().isEmpty()){
+            throw new MissingParameterException("Full Name cannot be Empty");
+        }
+
+        //Empty email error handling
+        if(updatedCustomer.getCustomerEmail().isEmpty()){
+            throw new MissingParameterException("Email cannot be Empty");
+        }
+
+        //Empty mobile number error handling
+        if(updatedCustomer.getCustomerMobileNumber().isEmpty()){
+            throw new MissingParameterException("Mobile Number cannot be Empty");
+        }
+
+        //Empty password error handling
+        if(updatedCustomer.getCustomerPassword().isEmpty()){
+            throw new MissingParameterException("Password cannot be Empty");
+        }
+
+        //Empty street name error handling
+        if(updatedCustomer.getCustomerAddressStreet().isEmpty()){
+            throw new MissingParameterException("Address Street cannot be Empty");
+        }
+
+        //Empty city name error handling
+        if(updatedCustomer.getCustomerAddressCity().isEmpty()){
+            throw new MissingParameterException("Address City cannot be Empty");
+        }
+
+        //Empty district error handling
+        if(updatedCustomer.getCustomerAddressDistrict().isEmpty()){
+            throw new MissingParameterException("Address District cannot be Empty");
+        }
+
+        //Empty NIC number error handling
+        if(updatedCustomer.getCustomerNIC().isEmpty()){
+            throw new MissingParameterException("NIC Number cannot be Empty");
+        }
+
+        Customer existingCustomer = customer.get();
+
+        existingCustomer.setCustomerFullName(updatedCustomer.getCustomerFullName());
+        existingCustomer.setCustomerEmail(updatedCustomer.getCustomerEmail());
+        existingCustomer.setCustomerMobileNumber(updatedCustomer.getCustomerMobileNumber());
+        existingCustomer.setCustomerPassword(updatedCustomer.getCustomerPassword());
+        existingCustomer.setCustomerAddressStreet(updatedCustomer.getCustomerAddressStreet());
+        existingCustomer.setCustomerAddressCity(updatedCustomer.getCustomerAddressCity());
+        existingCustomer.setCustomerAddressDistrict(updatedCustomer.getCustomerAddressDistrict());
+        existingCustomer.setCustomerNIC(updatedCustomer.getCustomerNIC());
+        existingCustomer.setCustomerDateOfBirth(updatedCustomer.getCustomerDateOfBirth());
+
+        return customerRepository.save(existingCustomer);
     }
 
     //delete customer
     public void deleteCustomer(Long id){
         Optional<Customer> customer = customerRepository.findById(id);
 
+        // Customer Id not found error handling
         if(customer.isEmpty()){
             throw new ResourceNotFoundException("Customer with ID " + id + " not found.");
         }
