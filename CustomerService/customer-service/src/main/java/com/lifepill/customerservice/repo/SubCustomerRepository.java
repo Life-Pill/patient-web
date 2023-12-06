f282bd99640c32260e92b7e867a0d9a437f9525c
Inupa Vihan <inupavihan9@gmail.com>
2023-12-06 14:53:21 +0530

    Add Sub customer entity

---
+++ CustomerService/customer-service/src/main/java/com/lifepill/customerservice/repo/SubCustomerRepository.java
@@ -0,0 +1,8 @@
+package com.lifepill.customerservice.repo;
+
+import com.lifepill.customerservice.model.SubCustomer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SubCustomerRepository extends JpaRepository<SubCustomer, Long> {
+
+}
