eb97bab03c321371d92bb35935b3cac2b77b33a9
Inupa Vihan <inupavihan9@gmail.com>
2023-12-20 23:04:16 +0530

    Update route for medical records

--- CustomerService/customer-service/src/main/java/com/lifepill/customerservice/controller/MedicalRecordController.java
+++ CustomerService/customer-service/src/main/java/com/lifepill/customerservice/controller/MedicalRecordController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("medicalRecords")
+@RequestMapping("/medicalRecords")
 public class MedicalRecordController {
     @Autowired
     private MedicalRecordService medicalRecordService;
