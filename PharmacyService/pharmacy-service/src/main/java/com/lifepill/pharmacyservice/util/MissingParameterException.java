5534cec0c17a4d05ca373bcbdc56d174c9cbf6b8
Inupa Vihan <inupavihan9@gmail.com>
2024-03-12 20:47:06 +0530

    Remove Pharmacy service API files

--- PharmacyService/pharmacy-service/src/main/java/com/lifepill/pharmacyservice/util/MissingParameterException.java
+++
@@ -1,7 +0,0 @@
-package com.lifepill.pharmacyservice.util;
-
-public class MissingParameterException extends RuntimeException {
-    public MissingParameterException(String message) {
-        super(message);
-    }
-}
