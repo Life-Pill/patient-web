49504d4bf557e8e7d32241ed2a33e5be0a51b096
Inupa Vihan <inupavihan9@gmail.com>
2024-07-10 21:28:13 +0530

    Add endpoint for payment confirmation

--- CustomerService/customer-service/src/main/java/com/lifepill/customerservice/controller/PaymentController.java
+++ CustomerService/customer-service/src/main/java/com/lifepill/customerservice/controller/PaymentController.java
@@ -4,6 +4,9 @@ import com.lifepill.customerservice.model.PaymentRequest;
 import com.lifepill.customerservice.service.StripeService;
 import com.stripe.exception.StripeException;
 import com.stripe.model.PaymentIntent;
+
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,5 +20,12 @@ public class PaymentController {
     @PostMapping("/paymentIntent")
     public PaymentIntent createPaymentIntent(@RequestBody PaymentRequest paymentRequest) throws StripeException {
         return stripeService.createPaymentIntent(paymentRequest.getAmount(), paymentRequest.getCurrency());
+    }
+
+    @PostMapping("/confirmPayment")
+    public PaymentIntent confirmPayment(@RequestBody Map<String, String> paymentData) throws StripeException {
+        String paymentIntentId = paymentData.get("paymentIntentId");
+        PaymentIntent paymentIntent = PaymentIntent.retrieve(paymentIntentId);
+        return paymentIntent.confirm();
     }
 }
