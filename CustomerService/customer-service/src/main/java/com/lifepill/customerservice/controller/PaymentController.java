6568a14a95c0be106475f9a353239165c35f4e2a
Inupa Vihan <inupavihan9@gmail.com>
2024-07-10 21:53:09 +0530

    Update API documentation

--- CustomerService/customer-service/src/main/java/com/lifepill/customerservice/controller/PaymentController.java
+++ CustomerService/customer-service/src/main/java/com/lifepill/customerservice/controller/PaymentController.java
@@ -5,23 +5,30 @@ import com.lifepill.customerservice.service.StripeService;
 import com.stripe.exception.StripeException;
 import com.stripe.model.PaymentIntent;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/payment")
+@RequestMapping("/payment")
 public class PaymentController {
 
     @Autowired
     private StripeService stripeService;
 
+    @Tag(name = "post", description = "POST methods of Customer service API")
+    @Operation(description = "Create payment intent")
     @PostMapping("/paymentIntent")
     public PaymentIntent createPaymentIntent(@RequestBody PaymentRequest paymentRequest) throws StripeException {
         return stripeService.createPaymentIntent(paymentRequest.getAmount(), paymentRequest.getCurrency());
     }
 
+    @Tag(name = "post", description = "POST methods of Customer service API")
+    @Operation(description = "Confirm payment")
     @PostMapping("/confirmPayment")
     public PaymentIntent confirmPayment(@RequestBody Map<String, String> paymentData) throws StripeException {
         String paymentIntentId = paymentData.get("paymentIntentId");
