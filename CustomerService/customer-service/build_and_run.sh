c95bed1c1111689f7dd27e59877fa0ab94fc8c59
Pramitha Jayasooriya <lpramithamj@gmail.com>
2024-10-21 02:10:06 +0530

    update

--- CustomerService/customer-service/build_and_run.sh
+++ CustomerService/customer-service/build_and_run.sh
@@ -10,6 +10,6 @@ docker stop lifepill-customer-service || true
 docker rm lifepill-customer-service || true
 
 # Run the Docker container
-docker run -d -p 8081:8081 --name lifepill-customer-service lifepill-customer-service:latest
+docker run -d -p 8076:8076 --name lifepill-customer-service lifepill-customer-service:latest
 
-echo "Container lifepill-customer-service is running and accessible on port 8081"
+echo "Container lifepill-customer-service is running and accessible on port 8076"
